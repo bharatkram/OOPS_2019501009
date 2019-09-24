0c06e439ed396f58fec8a35f9a7dd0e7f06f571c
bharatkram <bharat.bharatkram@msitprogram.net>
2019-09-24 16:38:59 +0530

    Assignment 8 final

--- Day-2/Assignment-8/LoneTeen.java
+++ Day-2/Assignment-8/LoneTeen.java
@@ -27,5 +27,14 @@ public class LoneTeen {
      */
     public static boolean loneTeen(int a, int b) {
         // Your code goes here....
+        if ((a>12 && a<20) && (b<=12 || b>=20))
+        {
+            return true;
+        }
+        else if ((b>12 && b<20) && (a<=12 || a>=20))
+        {
+            return true;
+        }
+        return false;
     }
 }
