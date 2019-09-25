8474c0044b3ae10e97e2eb523d6f13886a17f571
bharatkram <bharat.bharatkram@msitprogram.net>
2019-09-25 18:37:53 +0530

    assignment 4 final after checkstyle

--- Day-3/Assignment-3/Squares.java
+++ Day-3/Assignment-3/Squares.java
@@ -1,29 +1,38 @@
 /**
  * Given an array of Integers, return an array of the square
- * of each values in the array. 
- * 
+ * of each values in the array.
+ *
  * Input : {1, 2, 3}
  * Output : {1, 4, 9}
- * 
+ *
  * @author Siva Sankar
  * @author Mayank
+ * @author Bharat Ram Koppu
  */
 
-public class Squares {
+public final class Squares {
+    /**
+     * Empty Constructor.
+    */
+    private Squares() {
+        //Empty Constructor.
+    }
 
     /**
      * This method returns the array of ints that are squares of each element
      * in the input array.
-     * 
-     * @param nums, the input array.
-     * 
+     *
+     * @param nums the input array.
+     *
      * @return the array of ints that are squares of each element
      * in the input array.
-     * 
+     *
      */
-    public static int[] squared(int[] nums) {
+    public static int[] squared(final int[] nums) {
         //  Your code goes here.
-        
-        return null;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        return nums;
     }
 }
--- Day-3/Assignment-4/StringHandling.java
+++ Day-3/Assignment-4/StringHandling.java
@@ -1,23 +1,32 @@
 /**
- * Given an array of Strings, convert each string of the array into 
- * lower case String and then return the array. 
- * 
+ * Given an array of Strings, convert each string of the array into
+ * lower case String and then return the array.
+ *
  * Input : {"Hello", "Hi"}
  * Output : {"hello", "hi"}
- * 
+ *
  * @author Siva Sankar
+ * @author Bharat Ram Koppu
  */
 
-public class StringHandling {
+public final class StringHandling {
+    /**
+     * Empty Constructor.
+    */
+    private StringHandling() {
+        //Empty Constructor.
+    }
 
     /**
      * This method returns the array of lower case strings.
-     * @param arr, the input array.
+     * @param arr the input array.
      * @return the array of lower case Strings based on the input parameter.
      */
-    public static String[] lower(String[] arr) {
+    public static String[] lower(final String[] arr) {
         //  your code goes here....
-        
-        return null;
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i].toLowerCase();
+        }
+        return arr;
     }
 }
