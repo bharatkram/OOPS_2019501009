fe9679950fd8d7d208f855dc256b7b52d04d5ad7
bharatkram <bharat.bharatkram@msitprogram.net>
2019-09-25 17:12:04 +0530

    assignment 1 final

--- Day-3/Assignment-1/StringTimes.java
+++ Day-3/Assignment-1/StringTimes.java
@@ -15,7 +15,7 @@ public class StringTimes {
     /**
      * Empty constructor.
     */
-    public void StringTimes() {
+    public int StringTimes() {
         // Empty constructor.
     }
     /**
