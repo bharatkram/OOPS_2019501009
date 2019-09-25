fb54993a9e648e4c152843e222547ebddc8f3f36
bharatkram <bharat.bharatkram@msitprogram.net>
2019-09-25 17:09:34 +0530

    assignment 1 final

--- Day-3/Assignment-1/StringTimes.java
+++ Day-3/Assignment-1/StringTimes.java
@@ -1,16 +1,23 @@
 /**
  * Given a string and an integer value n, repeat the string and concatenate 
  * to the resultant string n times.
- * 
+ *
  * Input : Hi, 2
  * Output : HiHi
- * 
+ *
  * You can see the more test cases in the JUnit.java file.
- * 
+ *
  * @author Siva Sankar
+ * @author Bharat Ram Koppu
  */
 
 public class StringTimes {
+    /**
+     * Empty constructor.
+    */
+    public void StringTimes() {
+        // Empty constructor.
+    }
     /**
      * This method returns the String that is appending the String n 
      * times
@@ -18,9 +25,12 @@ public class StringTimes {
      * @param  n   the integer
      * @return the String that is appended n times to the resultant Stirng
      */
-    public static String stringTimes(String str, int n) {
+    public static String stringTimes(final String str, final int n) {
         //  Your code goes here....
-        //  
-        return "";
+        String retStr = "";
+        for (int i = 0; i < n; i++) {
+            retStr = retStr + str;
+        }
+        return retStr;
     }
 }
