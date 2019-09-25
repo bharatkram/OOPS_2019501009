cc909f282d393e75a6e5ea8e120ae10d1e47ffc8
bharatkram <bharat.bharatkram@msitprogram.net>
2019-09-25 18:00:21 +0530

    assignmnet 2 final

--- Day-3/Assignment-2/FrontTimes.java
+++ Day-3/Assignment-2/FrontTimes.java
@@ -1,21 +1,28 @@
 /**
  * Given a string and a non-negative int n, we'll say that the front
- * of the string is the first 3 chars, or whatever is there if the 
+ * of the string is the first 3 chars, or whatever is there if the
  * string is less than length 3. Return n copies of the front;
- * 
+ *
  * Input : frontTimes("Chocolate", 2)
  * Output : ChoCho
- * 
+ *
  * Input : frontTimes("Chocolate", 3)
  * Output : ChoChoCho
- * 
+ *
  * Input : frontTimes("Abc", 3)
  * Output : AbcAbcAbc
- * 
+ *
  * @author: Siva Sankar
+ * @author: Bharat Ram Koppu
  */
 
-public class FrontTimes {
+public final class FrontTimes {
+    /**
+    * Empty Constructor.
+    */
+    private FrontTimes() {
+        //Empty Constructor.
+    }
 
     /**
      * This method returns n copies of the front.
@@ -23,9 +30,21 @@ public class FrontTimes {
      * @param n, the non-negative integer value.
      * @return the n copies of the front string.
      */
-    public static String frontTimes(String str, int n) {
+    public static String frontTimes(final String str, final int n) {
         //  Your code goes here....
-        
-        return "";
+        if (str.length() >= 3) {
+            String subStr = str.substring(0, 3);
+            String retStr = "";
+            for (int i = 0; i < n; i++) {
+                retStr = retStr + subStr;
+            }
+        return retStr;
+        }else {
+            String retStr = "";
+            for (int i = 0; i < n; i++) {
+                retStr = retStr + str;
+            }
+            return retStr;
+        }
     }
 }
