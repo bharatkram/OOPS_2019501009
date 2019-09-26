eda466bc08f87b8e9e23083f695b8994f432c6de
bharatkram <bharat.bharatkram@msitprogram.net>
2019-09-26 15:51:56 +0530

    assignments

--- Day-3/Assignment-6/Stats.java
+++ Day-3/Assignment-6/Stats.java
@@ -1,79 +1,91 @@
 /**
  * Given an array of integers Find the mean, median, mode, standard deviation
  * and variance.
- * 
+ *
  * Mean
  * Input : [1,2,3,4,5]
  * Output : 3.0
- * 
+ *
  * Median
  * Input : [1,2,3,4,5]
  * Output : 3.0
- * 
+ *
  * Mode
  * Input : [1,1,2,2,3]
  * Output : [1,2]
- * 
+ *
  * Variance
  * Input : [1,2,3,4,5]
  * Output : 2.0
- * 
+ *
  * Standard Deviation
  * Input : [1,2,3,4,5]
  * Output : 1.41421356
- * 
+ *
  * @author Siva Sankar
+ * @author Bharat Ram Koppu
  */
 
 import java.util.*;
 
-public class Stats {
+public final class Stats {
+    /**
+     * Empty Constructor.
+    */
+    private Stats() {
+        //Empty Constructor
+    }
 
     /**
      * This method should return the mean of the elements of the array.
      * If the arr is empty (length is 0), return the mean as 0.
-     * @param arr, the input array contians elements.
+     * @param arr the input array contians elements.
      * @return the mean of the array.
      */
-    public static double mean(int[] arr) {
+    public static double mean(final int[] arr) {
         //  Your code goes here....
-
-        return 0.0;
+        double sum = 0.0
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum/arr.length;
     }
 
     /**
      * This method returns the median of the elements of the array.
      * Prerequisite : The array should be sorted to find the median.
-     * 
+     *
      * Hint : Arrays.sort(arr) sorts the elements of the array.
-     * 
-     * @param arr, the input array contains random elements (not in sorted order)
+     *
+     * @param arr the input array contains random elements (not in sorted order)
      * @return the median of the elements of the array.
      */
     public static double median(int[] arr) {
         //  Your code goes here...
-
-        return 0.0;
+        if (arr.length %2 != 0) {
+            return arr[arr.length/2];
+        }
+        return (arr[arr.length/2] + arr[(arr.length/2) - 1]) / 2.0;
     }
 
     /**
      * This method returns the mode of the array. If there is no mode, then it returns
      * 0 as the mode.
-     * 
-     * @param arr, the input array.
+     *
+     * @param arr the input array.
      * @return the mode of the array, 0 otherwise if there is no mode.
      */
     public static int mode(int[] arr) {
         //  Your code goes here....
-
+        
         return 0;
     }
 
     /**
      * This method returns the variance of the input array.
      * Make use of the mean functon which has already written.
-     * 
-     * @param arr, the input array
+     *
+     * @param arr the input array
      * @return the variance of the array.
      */
     public static double variance(int[] arr) {
@@ -85,7 +97,7 @@ public class Stats {
     /**
      * This method returns the standard deviation of the array.
      * Hint : You can find the square root using Math.sqrt() method.
-     * @param arr, the input array
+     * @param arr the input array
      * @return the standard deviation of the input array.
      */
     public static double standardDeviation(int[] arr) {
