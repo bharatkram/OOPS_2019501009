97862dac38e8ee6afd53f67343cd7860633e5062
bharatkram <bharat.bharatkram@msitprogram.net>
2019-09-30 15:00:29 +0530

    assignment 1 final after checkstyle

--- Day-7/Assignment-1/Power.java
+++ Day-7/Assignment-1/Power.java
@@ -1,30 +1,37 @@
 /**
- * Given "a" an integer or float value, and b as an integer value, 
+ * Given "a" an integer or float value, and b as an integer value,
  * Find a power b.
  *
  * @author Siva Sankar
  */
 
  public class Power {
-    /**
-     * This method finds a^b where both a and b are integers.
-     * @param  a, the base
-     * @param  b, the exponential
-     * @return a^b which is a long value.
-     */
-     public static long pow(int a, int b) {
-        return -1;
-     }
+   /**
+    * This method finds a^b where both a and b are integers.
+    * @param  a the base
+    * @param  b the exponential
+    * @return a^b which is a long value.
+    */
+   public static long pow(final int a, final int b) {
+      long answer = 1;
+      for (int i = 0; i < b; i++) {
+         answer *= a;
+      }
+      return answer;
+   }
 
-     /**
-     * This method finds a^b where both variable a is a double 
-     * and b is an integer.
-     * @param  a, the base
-     * @param  b, the exponential
-     * @return a^b which is a long value.
-     */
-     public static double pow(float a, int b) {
-        return -1;
-        
-     }
- }
+   /**
+    * This method finds a^b where both variable a is a double
+    * and b is an integer.
+    * @param  a the base
+    * @param  b the exponential
+    * @return a^b which is a long value.
+    */
+   public static double pow(final float a, final int b) {
+      double answer = 1;
+      for (int i = 0; i < b; i++) {
+         answer *= a;
+      }
+      return answer;
+   }
+}
