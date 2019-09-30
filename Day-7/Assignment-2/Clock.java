20d6391c6148ead799a5f7d50fb21432b423e677
bharatkram <bharat.bharatkram@msitprogram.net>
2019-09-30 15:27:52 +0530

    assignment 2

--- Day-7/Assignment-2/Clock.java
+++ Day-7/Assignment-2/Clock.java
@@ -8,6 +8,12 @@
  */
 
 public class Clock {
+    // attribute for hours.
+    int h;
+
+    // attribute for minutes.
+    int m;
+
     // Creates a clock whose initial time is h hrs and m min.
     /**
      * The parameterised constructor will instatiates the object with the values of
@@ -16,23 +22,34 @@ public class Clock {
      * @param m
      */
     public Clock(int h, int m) {
-
+        this.h = h;
+        this.m = m;
     }
 
     // Creates a clock whose initial time is specified as a string, using the format HH:MM.
 
     public Clock(String s) {
-
+        String[] str = s.split(":");
+        h = (int) str[0];
+        m = (int) str[1];
     }
 
     // Returns a string representation of this clock, using the format HH:MM.
     public String toString() {
-
+        String str = ("%2d:%2d",h,m);
+        return str;
     }
 
     // Is the time on this clock earlier than the time on that one?
     public boolean isEarlierThan(Clock that) {
-        
+        if(that.h < this.h)
+            return true;
+        else if(that.h > this.h)
+            return false;
+        else if(that.m > this.h)
+            return true;
+        else
+            return false;
     }
 
     private void check() {
@@ -41,12 +58,19 @@ public class Clock {
 
     // Adds 1 minute to the time on this clock.
     public void tic() {
-        
+        m += 1;
     }
 
     // Adds Δ min to the time on this clock.
     public void toc(int delta) {
-        
+        m += delta;
+        if(m >= 60) {
+            m -= 60;
+            h += 1;
+            if(h >= 24) {
+                h -= 24;
+            }
+        }
     }
 
     // Test client (see below).
