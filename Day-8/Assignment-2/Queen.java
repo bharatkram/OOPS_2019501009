f6c4337822710f1e245a44e587949ffb3e39363f
bharatkram <bharat.bharatkram@msitprogram.net>
2019-10-01 19:56:47 +0530

    assignment 2 final

---
+++ Day-8/Assignment-2/Queen.java
@@ -0,0 +1,44 @@
+class Piece {
+    public boolean isValid(int x, int y) {
+        System.out.println("Inside Base Class");
+        if (x < 8 && y< 8) {
+            return true;
+        }
+        return false;
+    } 
+}
+
+class QueenPiece extends Piece {
+    int x = 2;
+    int y = 2;
+    public boolean isValid(int x, int y) {
+        super.isValid(x, y);
+        System.out.println("Inside Derived Class");
+        if (this.x == x) {
+            return true;
+        } else if(this.y == y) {
+            return true;
+        } else if(this.x + this.y == x + y) {
+            return true;
+        } else if(this.x - x == this.y - y) {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class Queen {
+    public static void main(String[] args) {
+        QueenPiece q = new QueenPiece();
+        if(q.isValid(0, 0)) {
+            System.out.println("Valid move");
+        } else {
+            System.out.println("Invalid move");
+        }
+        if(q.isValid(3, 4)) {
+            System.out.println("Valid move");
+        } else {
+            System.out.println("Invalid move");
+        }
+    }
+}
