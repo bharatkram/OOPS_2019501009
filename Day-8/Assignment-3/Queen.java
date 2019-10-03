991010828a91268341b04efacac8597ef437868c
bharatkram <bharat.bharatkram@msitprogram.net>
2019-10-03 09:37:14 +0530

    assignment final

--- Day-8/Assignment-3/Queen.java
+++ Day-8/Assignment-3/Queen.java
@@ -9,18 +9,29 @@ class Piece {
 }
 
 class QueenPiece extends Piece {
+    
     int x = 2;
     int y = 2;
+
+    public void changePos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public boolean isValid(int x, int y) {
         if (super.isValid(x, y)) {
             System.out.println("Inside Derived Class");
             if (this.x == x) {
+                changePos(x, y);
                 return true;
             } else if(this.y == y) {
+                changePos(x, y);
                 return true;
             } else if(this.x + this.y == x + y) {
+                changePos(x, y);
                 return true;
             } else if(this.x - x == this.y - y) {
+                changePos(x, y);
                 return true;
             }
             return false;
@@ -34,12 +45,12 @@ public class Queen {
     public static void main(String[] args) {
         QueenPiece q = new QueenPiece();
         if(q.isValid(0, 0)) {
-            System.out.println("Valid move");
+            System.out.println("Position updated.");
         } else {
             System.out.println("Invalid move");
         }
         if(q.isValid(3, 4)) {
-            System.out.println("Valid move");
+            System.out.println("Position updated.");
         } else {
             System.out.println("Invalid move");
         }
